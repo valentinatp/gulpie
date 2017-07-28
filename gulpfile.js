de8d35cf42597655b829f88370f655426751ad82
Valentina Toledo <valentina.toledoporma@gmail.com>
2017-07-28 10:25:59 -0400

    realizado bien

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var uglify = require('gulp-sass');
-var uglify = require('gulp-minify-css');
-var uglify = require('gulp-webserver');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var webserver = require('gulp-webserver');
 
 gulp.task('script', function(){
 	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js',
